5d2146249112ecafa735f2e6a417992c8cb6b904
StanislavKlyuev <patriek@rambler.ru>
2025-10-01 11:51:12 +0300

    CourseWork

---
+++ src/Employee.java
@@ -0,0 +1,87 @@
+public class Employee {
+    private String surname;
+    private String name;
+    private String patronymic;
+    private int department;
+    private int salary;
+    private int id;
+    private static int idCounter;
+
+    // переменные для красивого вывода информаци :)
+    public static char rub = '₽';
+    public static final String BLU = "\u001B[34m";
+    public static final String RES = "\u001B[0m";
+
+    public Employee(String surname, String name, String patronymic, int department, int salary) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.department = department;
+        this.salary = salary;
+        ++idCounter;
+    }
+
+    /* это метод на случай, если какого-нибуди сотрудника нужно уволить, а на его место взять нового
+     и присвоить ему свободный id.
+     Так же для увольнении сотрудников нужно написать метод для уменьшения idCounter. Но это уже другая история. */
+    public void setId(int idnum) {
+        this.id = idnum;
+    }
+
+    public static int idCounte() {
+        return idCounter;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public int getDepartment() {
+        return this.department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+
+    // Переопределение сравнения сотрудников по зарплате
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Employee employee = (Employee) object;
+        return employee.salary == salary;
+    }
+
+    // Вывод полных данных сотрудника
+    @Override
+    public String toString() {
+        return "id # " + id + ": ФАМИЛИЯ: " + BLU + surname + RES + "; ИМЯ: " + BLU + name + RES + "; ОТЧЕСТВО: " + BLU + patronymic + RES + "; ОТДЕЛ: " + BLU + department + RES + "; ЗАРПЛАТА: " + BLU + salary + rub + RES;
+    }
+
+    // Вывод сокращенной информации
+    public void printShortInfo() {
+        System.out.println("Сокращенные данные: " + "id # " + id + ": ФАМИЛИЯ: " + BLU + surname + RES + " ИМЯ: " + BLU + name + RES + "; ЗАРПЛАТА: " + BLU + salary + rub + RES);
+    }
+}
---
+++ src/EmployeeBook.java
@@ -0,0 +1,153 @@
+public class EmployeeBook {
+    public static Employee[] employees;
+
+    // переменные для красивого вывода информаци :)
+    public static final String BLU = "\u001B[34m";
+    public static final String RES = "\u001B[0m";
+    public static char rub = '₽';
+
+    // Создаем новую книгу (массив) сотрудников
+    public static void setEmployees(int size) {
+        Employee[] empl = new Employee[size];
+        employees = empl;
+    }
+
+    /* Добавляем нового сотрудника
+    Если принципиально реализовать через boolean (TRUE или FALSE) и поставить галочку в критериях оценки, переделать не составит труда,
+    но с выводом успешного результата интереснее))
+     */
+    public static void addEmployee(String surname, String name, String patron, int dept, int salary) {
+        Employee employee = new Employee(surname, name, patron, dept, salary);
+        int i = 0;
+        for (Employee emp : employees) {
+            if (emp == null) {
+                emp = employee;
+                employees[i] = employee;
+                emp.setId(i + 1);
+                System.out.println("Вы успешно добавили нового сотрудника.");
+                break;
+            } else {
+                i++;
+            }
+        }
+        if (i == employees.length)
+            System.out.println("Вы пытались добавить нового сотрудника. Увы, вакантных мест нет.");
+    }
+
+    // список всех сотрудников
+    public static void printAllEmployees() {
+        System.out.println("Список всех сотрудников:");
+        for (Employee emp : employees) {
+            if (emp == null)
+                continue;
+            System.out.println(emp);
+        }
+        System.out.println("Общее количесвто сотрудников: " + Employee.idCounte());
+    }
+
+    // среднее значение зарплаты
+    public static void printAverageSalary() {
+        int sum = 0;
+        int count = 0;
+        for (Employee emp : employees) {
+            if (emp == null)
+                continue;
+            sum += emp.getSalary();
+            count++;
+        }
+        System.out.println("Средняя зарплата сотрудников: " + String.format("%.2f", (sum * 1.0 / count)) + rub);
+    }
+
+    // вывод налога
+    public static void printTax(String tax) {
+        System.out.println("Данные по налогам:");
+        switch (tax) {
+            case "PROPORTIONAL":
+                int nal = 13;
+                for (Employee emp : employees) {
+                    if (emp == null)
+                        continue;
+                    System.out.println("Налог сотрудника " + BLU + emp.getSurname() + RES + " составляет " + nal + "% и равен " + BLU + String.format("%.2f", emp.getSalary() * nal * 1.0 / 100) + rub + RES);
+                }
+                break;
+            case "PROGRESSIVE":
+                for (Employee emp : employees) {
+                    if (emp == null)
+                        continue;
+                    if (emp.getSalary() < 150)
+                        nal = 13;
+                    else if (emp.getSalary() < 350)
+                        nal = 17;
+                    else
+                        nal = 21;
+                    System.out.println("Налог сотрудника " + BLU + emp.getSurname() + RES + " составляет " + nal + "% и равен " + BLU + String.format("%.2f", emp.getSalary() * nal * 1.0 / 100) + rub + RES);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    // индексация зарплаты сотрудникам отдела
+    public static void getDept(int dep, int index) {
+        System.out.println("Индексация зарплат:");
+        for (Employee emp : employees) {
+            if (emp == null || emp.getDepartment() != dep)
+                continue;
+            else {
+                int increase = emp.getSalary() * index / 100;
+                emp.setSalary(emp.getSalary() + increase);
+                System.out.println("Зарплата сотрудника " + BLU + emp.getSurname() + RES + " увеличина на " + increase + rub + " и составляет " + BLU + emp.getSalary() + rub + RES);
+            }
+        }
+    }
+
+    // вывод первого сотрудника с зарплатой выше порога
+    public static void getEmploy(int dep, int sal) {
+        System.out.println("Сотрудник отдела " + dep + ", которому переплачивают:");
+        for (Employee emp : employees) {
+            if (emp == null || emp.getDepartment() != dep || emp.getSalary() <= sal)
+                continue;
+            else {
+                emp.printShortInfo();
+                break;
+            }
+        }
+    }
+
+    // вывод N сотрудников c зарплатой ниже порога
+    public static void getLowEmployees(int wage, int employeeNumber) {
+        System.out.println("Сотрудники, которым не доплачивают:");
+        int i = 0;
+        for (Employee emp : employees) {
+            if (emp == null || emp.getSalary() >= wage)
+                continue;
+            while (i < employeeNumber) {
+                emp.printShortInfo();
+                i++;
+                break;
+            }
+        }
+    }
+
+    public static boolean findEmployee(Employee employee) {
+        System.out.println("Проверка по бух.учету");
+        boolean have = false;
+        for (Employee emp : employees) {
+            if (emp.equals(employee)) {
+                have = true;
+                break;
+            }
+        }
+        return have;
+    }
+
+    public static Employee getEmployeeId(int id) {
+        System.out.println("Соотрудник по id # " + id);
+        for (Employee emp : employees) {
+            if (id == emp.getId())
+                return emp;
+        }
+        return null;
+    }
+}
